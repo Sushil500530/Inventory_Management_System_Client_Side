038a4cc78172e03f7fabcf3f5659ad7abdb96a49
Sushil97 <sushil500530@gmail.com>
2023-11-27 00:03:09 +0600

    add add product and show card category

---
+++ src/Pages/Home/Shop/Collection.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/prop-types */
+import { MdDetails } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+const Collection = ({ product }) => {
+    const { _id, product_name, product_cost, image, discount, description, location, owner_name } = product || {};
+    return (
+        <div className="card bg-base-100 shadow-xl">
+            <figure className=' w-full h-[300px]'>
+                <img src={image} alt="image" className='w-full h-full rounded-md' />
+            </figure>
+            <div className="flex justify-between items-center px-5">
+                <div className="flex gap-3 mt-3">
+                    <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="host-image" className="rounded-full w-12" />
+                    <span>
+                        <p className="font-medium">{owner_name}</p>
+                        <p className="font-medium">{location}</p>
+                    </span>
+                </div>
+                <div>
+                    <span>
+                        <p className="text-[18px] font-medium">$ {product_cost}</p>
+                        <p>Discount {discount}%</p>
+                    </span>
+                </div>
+            </div>
+            <div className="p-5">
+                <h2 className="card-title">{product_name}</h2>
+                <p>{description.slice(0, 70)}</p>
+                <div className="card-actions w-2/3 mx-auto mt-5">
+                    <Link to={`/product-details${_id}`} className="w-full">
+                        <button className="btn bg-gradient-to-r from-purple-500 to-pink-500 text-[18px] w-full hover:text-white ">View Details <MdDetails className="text-3xl" /></button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Collection;
--- src/Pages/Home/Shop/ShopCollection.jsx
+++ src/Pages/Home/Shop/ShopCollection.jsx
@@ -1,47 +1,18 @@
+import useAllProduct from '../../../Hooks/useAllProduct';
 import image from '../../../assets/image/banner/1cf32d0c7f6a19ef652cc468f576ae25.jpg'
+import Loading from '../../../components/Shared/Loading';
+import Collection from './Collection';
 const ShopCollection = () => {
+    const [allProducts, , isLoading] = useAllProduct();
+    console.log(allProducts);
+    if (isLoading) {
+        return <Loading />
+    }
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 my-12">
-            <div className="card bg-base-100 shadow-xl">
-                <figure className=' w-full h-[300px]'><img src={image} alt="Shoes" className='w-full h-full' /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card bg-base-100 shadow-xl">
-                <figure className=' w-full h-[300px]'><img src={image} alt="Shoes" className='w-full h-full' /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card bg-base-100 shadow-xl">
-                <figure className=' w-full h-[300px]'><img src={image} className='w-full h-full' alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card bg-base-100 shadow-xl"> 
-                <figure className=' w-full h-[300px]'><img src={image} className='w-full h-full' alt="Shoes" /></figure>
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
-            </div>
+        <div className="p-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 my-12">
+           {
+            allProducts?.map(product => <Collection key={product?._id} product={product} /> )
+           }
         </div>
     );
 };
